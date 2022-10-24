9b3b8765b350d90cfe38836fc9e637d73fb4b6ac
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-24 21:44:53 +0200

    Fix survey page

--- apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
+++ apps/surveychimp-survey-spa/src/app/pages/SurveyPage.tsx
@@ -2,7 +2,10 @@ import { useParams } from 'react-router-dom';
 
 const SurveyPage = () => {
     const { surveyId } = useParams();
-    return (<h1>{surveyId}</h1>)
+    return (<>
+        <h1>Survey</h1>
+        <p>{surveyId}</p>
+    </>)
 }
 
 export default SurveyPage;
