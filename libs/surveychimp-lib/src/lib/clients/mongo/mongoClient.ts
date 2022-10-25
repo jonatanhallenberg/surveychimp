dbe855917f000a6e0e3f06f83fe244ae805f60a9
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-25 10:21:39 +0200

    change mongo conn str

--- libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
@@ -2,6 +2,6 @@
 import mongoose from 'mongoose';
 
 export const connect = async (): Promise<typeof mongoose> => {
-    const connectionString = "mongodb://localhost:27017/forum123?readPreference=primary&ssl=false";
+    const connectionString = "mongodb+srv://jonatan:123qweasd@cluster0.otsv5.mongodb.net/?retryWrites=true&w=majority";
     return mongoose.connect(connectionString);
 }
