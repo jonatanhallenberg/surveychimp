b8f37fbd467b9b69cb5328fc31814c2380e29305
Markus <markus.mork@iths.se>
2022-10-27 13:25:45 +0200

    Add yaml

--- libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
@@ -2,6 +2,8 @@
 import mongoose from 'mongoose';
 
 export const connect = async (): Promise<typeof mongoose> => {
-    const connectionString = "mongodb+srv://jonatan:123qweasd@cluster0.otsv5.mongodb.net/?retryWrites=true&w=majority";
-    return mongoose.connect(connectionString);
+    if(!process.env.MONGO_DATABASE_URI) {
+        throw "500";
+    }
+    return mongoose.connect(process.env.MONGO_DATABASE_URI);
 }
