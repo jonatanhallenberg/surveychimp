58e3c480cb8bfec83016267c5faa4ea2641d8555
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-27 21:41:40 +0200

    remove mock code

--- apps/surveychimp-api/src/app/routes/surveyRouter.ts
+++ apps/surveychimp-api/src/app/routes/surveyRouter.ts
@@ -28,24 +28,41 @@ router.get(
 router.post('/survey',
     body("recipient.name").isString().isLength({ "min": 3 }).withMessage("Du måste skriva minst 3 tecken"),
     body("recipient.mobileNumber").isMobilePhone("sv-SE").withMessage("Skriv in ett mobilnummer, t.ex. 070 123 12 12"),
-    async (req, res) => {
+    async (req, res, next) => {
+        try {
+            //validation
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-        //validation
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            const survey = await addSurvey(req.body);
+            res.json(survey);
+        } catch (err) {
+            next(err);
         }
-
-        const survey = await addSurvey(req.body);
-        res.json(survey);
     });
 
-router.patch('/survey/:surveyId', async (req, res) => {
-    const survey = await addResponseToSurvey("6354fa3e707d56d9524fd2f3", {
-        rating: 3
+router.patch('/survey/:surveyId',
+    param("surveyId").isMongoId(),
+    body("rating").isNumeric().withMessage("Rating måste vara ett nummer"),
+    body("comment").isString().optional(),
+
+    async (req, res, next) => {
+        try {
+            //validation
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const survey = await addResponseToSurvey(req.params.surveyId, req.body);
+            res.json(survey);
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
     });
-    res.json(survey);
-});
 
 
 export default router;
--- libs/surveychimp-lib/src/lib/clients/mongo/survey/getSurveyById.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/survey/getSurveyById.ts
@@ -3,7 +3,7 @@ import SurveyModel, { ISurvey } from './SurveyModel';
 export const getSurveyById = async (surveyId: string) => {
     const survey = SurveyModel.findById(surveyId);
     if (!survey) {
-        throw 404;
+        throw "404";
     }
 
     return survey;
