7f5f79fd719ab8697a1cfb3c30279c11da5a1353
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-10-29 08:12:47 +0200

    deployment

---
+++ deploy/delete-environments.sh
@@ -0,0 +1,4 @@
+
+doctl databases delete surveychimp-db --force
+doctl apps delete surveychimp --force
+doctl apps delete surveychimp-dev --force
---
+++ deploy/setup-environments.sh
@@ -0,0 +1,19 @@
+SCRIPT_PATH=`pwd`
+echo $SCRIPT_PATH
+CLUSTER_NAME=surveychimp-db
+
+# Create database cluster
+doctl databases create $CLUSTER_NAME --engine mongodb --version 5 --region ams3
+
+# Get ID for database cluster
+CLUSTER_ID=$(doctl databases list | grep $CLUSTER_NAME | awk '{print $1}')
+
+# Create database for dev in cluster
+doctl databases db create $CLUSTER_ID dev
+
+# Create database for prod in cluster
+doctl databases db create $CLUSTER_ID prod
+
+#doctl apps create --spec ./surveychimp-app.yaml --format "ID"
+#doctl apps create --spec ./surveychimp-app-with-db.yaml
+
