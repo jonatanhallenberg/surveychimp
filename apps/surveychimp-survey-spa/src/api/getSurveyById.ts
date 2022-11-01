f176c151d554e6dbf13bd26bcf9ed287c981c79a
Linda Marko <lindamarko.dev@gmail.com>
2022-11-01 10:50:44 +0100

    Change base url

--- apps/surveychimp-survey-spa/src/api/getSurveyById.ts
+++ apps/surveychimp-survey-spa/src/api/getSurveyById.ts
@@ -1,7 +1,7 @@
 import { ISurvey } from '@surveychimp/surveychimp-lib';
 
 const getSurveyById = async (surveyId: string): Promise<ISurvey> => {
-    return (await fetch(`http://localhost:4200/api/survey/${surveyId}`)).json();
+    return (await fetch(`/api/survey/${surveyId}`)).json();
 }
 
 export default getSurveyById;
