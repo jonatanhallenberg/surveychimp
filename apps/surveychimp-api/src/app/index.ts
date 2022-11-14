e0fbf4e628375f926a6d89fd3c429e9c867fcd1e
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-11-14 13:31:58 +0100

    add sentry

--- apps/surveychimp-api/src/app/index.ts
+++ apps/surveychimp-api/src/app/index.ts
@@ -1,14 +1,39 @@
 import * as express from 'express';
-import {  handleGlobalErrors } from '@surveychimp/surveychimp-lib';
+import { handleGlobalErrors } from '@surveychimp/surveychimp-lib';
 import surveyRouter from './routes/surveyRouter';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const app = express();
+
+Sentry.init({
+    dsn: "https://975016e12f83420c81f2d1fa7e83cb17@o4504152294162432.ingest.sentry.io/4504157479895040",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 //Required to parse json body
 app.use(express.json());
 
 //Routers
 app.use(surveyRouter);
+
+app.use(Sentry.Handlers.errorHandler());
 
 //Global error handler
 app.use(handleGlobalErrors);
--- apps/surveychimp-survey-spa/src/main.tsx
+++ apps/surveychimp-survey-spa/src/main.tsx
@@ -1,8 +1,21 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 import App from './app/app';
+
+Sentry.init({
+    dsn: "https://722f548cbf6d4215ad8c49d543939cda@o4504152294162432.ingest.sentry.io/4504157459316736",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
