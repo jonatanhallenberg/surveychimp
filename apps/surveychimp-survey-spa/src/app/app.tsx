82679a0196ecd15c6e82f5c50f4b0029af5714fa
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-11-16 13:53:33 +0100

    add ga

--- apps/surveychimp-survey-spa/src/app/app.tsx
+++ apps/surveychimp-survey-spa/src/app/app.tsx
@@ -2,34 +2,32 @@
 import styled from 'styled-components';
 import SurveyPage from './pages/SurveyPage';
 import SurveyPageWithStars from './pages/SurveyPageWithStars';
-
 import StartPage from './pages/StartPage';
-
-
 import { Route, Routes } from 'react-router-dom';
-
-
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
 
 const StyledApp = styled.div`
     // Your style here
 `;
 
+const TRACKING_ID = "G-B7987J32K9"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 export function App() {
 
-  console.log('process.env.NX_RATING_STARS_FEATURE',  process.env.NX_RATING_STARS_FEATURE);
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <StyledApp>
       <Routes>
 
-        {process.env.NX_RATING_STARS_FEATURE === "true" ? <Route
+        <Route
           path="/survey/:surveyId"
           element={<SurveyPageWithStars />}
-        /> : <Route
-          path="/survey/:surveyId"
-          element={<SurveyPage />}
-        />}
+        />
 
         <Route
           path="/"
